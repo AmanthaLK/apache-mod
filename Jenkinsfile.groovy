e4ac95c58a5b7f616c6bd891f69fd3ecf69895fd
Amantha Amarasena <amantha.17@itfac.mrt.ac.lk>
2024-08-02 17:11:14 +0530

    changed indxhtml

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,19 +2,16 @@ pipeline {
     agent any
 
     stages {
-        stage('Build') {
+        stage('Test SSH Connection') {
             steps {
-                echo 'Starting Build Stage...'
-                echo 'Building...'
-                sh 'ls'                // List files and directories
-                sh 'pwd'               // Print the current working directory
-                sh 'cat index.html'    // Display the contents of index.html
-            }
-        }
-
-        stage('Clean Workspace') {
-            steps {
-                cleanWs() // Clean the workspace at the end of the pipeline
+                script {
+                    sshagent(['aws-personal']) {
+                        sh '''
+                            echo "Testing SSH connection..."
+                            ssh -o StrictHostKeyChecking=no ubuntu@172.31.82.42 "echo Connection successful"
+                        '''
+                    }
+                }
             }
         }
     }
