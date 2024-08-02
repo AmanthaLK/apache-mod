ba572bcdaf975038a17ff593a5167e7122a75917
Amantha Amarasena <amantha.17@itfac.mrt.ac.lk>
2024-08-02 15:16:09 +0530

    changed groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -6,39 +6,9 @@ pipeline {
             steps {
                 echo 'Starting Build Stage...'
                 echo 'Building...'
-                // Add your build steps here
+                sh 'ls' // This will run the 'ls' command
             }
-        }
-
-        stage('Test') {
-            steps {
-                echo 'Starting Test Stage...'
-                echo 'Testing...'
-                // Add your test steps here
-            }
-        }
-
-        stage('Deploy') {
-            steps {
-                echo 'Starting Deploy Stage...'
-                echo 'Deploying...'
-                // Add your deploy steps here
-            }
-        }
-
-        stage('Hello World') {
-            steps {
-                script {
-                    def message = 'Hello, World!'
-                    echo message
-                }
-            }
-        }
-    }
-
-    post {
-        always {
-            echo 'Pipeline completed.'
         }
     }
 }
+
