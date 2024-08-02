1802d6797aff7539e09cbb746f04c0205ac0844c
Amantha Amarasena <amantha.17@itfac.mrt.ac.lk>
2024-08-02 15:27:34 +0530

    added clean wd

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,6 +1,11 @@
 pipeline {
     agent any
 
+    options {
+        // Clean the workspace before running the pipeline
+        cleanWs()
+    }
+
     stages {
         stage('Build') {
             steps {
