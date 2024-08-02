6befc6fd0d04998872d3439406cbbdb496498598
Amantha Amarasena <amantha.17@itfac.mrt.ac.lk>
2024-08-02 15:29:27 +0530

    added clean ws

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,12 +2,6 @@ pipeline {
     agent any
 
     stages {
-        stage('Clean Workspace') {
-            steps {
-                cleanWs() // Clean the workspace before running the stages
-            }
-        }
-
         stage('Build') {
             steps {
                 echo 'Starting Build Stage...'
@@ -15,6 +9,12 @@ pipeline {
                 sh 'ls'                // List files and directories
                 sh 'pwd'               // Print the current working directory
                 sh 'cat index.html'    // Display the contents of index.html
+            }
+        }
+
+        stage('Clean Workspace') {
+            steps {
+                cleanWs() // Clean the workspace at the end of the pipeline
             }
         }
     }
